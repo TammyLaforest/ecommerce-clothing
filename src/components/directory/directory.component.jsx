6fc7dec27f47acbc9505e606030bdd6610e334c8
TammyLaforest <TammyLaforest@gmail.com>
2019-11-20 10:33:22 -0500

    Updated menu-items to use React Dom Router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,7 +12,8 @@ class Directory extends React.Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
+                    // linkUrl: 'shop/hats'
                 },
                 {
                     title: 'jackets',
@@ -48,7 +49,7 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {this.state.sections.map(
-                    ({ title, imageUrl, id, size }) => (<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />)
+                    ({ id, ...otherSectionProps }) => (<MenuItem key={id} {...otherSectionProps} />)
                 )}
             </div>
 
